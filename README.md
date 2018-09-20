c82a7d959babbe706d80a79e4f67f8097ca9c351
nils <nils@fedger.io>
2018-09-21 01:18:22 +0200

    first draft of mlm service

---
+++ README.md
@@ -0,0 +1,23 @@
+# is-this-an-mlm
+
+> Website that tells people whether a company is an MLM Scheme.
+
+## Build Setup
+
+``` bash
+# install dependencies
+$ npm install # Or yarn install
+
+# serve with hot reload at localhost:3000
+$ npm run dev
+
+# build for production and launch server
+$ npm run build
+$ npm start
+
+# generate static project
+$ npm run generate
+```
+
+For detailed explanation on how things work, checkout the [Nuxt.js docs](https://github.com/nuxt/nuxt.js).
+
---
+++ nuxt.config.js
@@ -0,0 +1,46 @@
+module.exports = {
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: 'is-this-an-mlm',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'Website that tells people whether a company is an MLM Scheme.' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+  /*
+  ** Customize the progress bar color
+  */
+  loading: { color: '#3B8070' },
+  /*
+  ** Bulma for Vue
+  */
+  modules: [
+    'nuxt-buefy',
+    ['nuxt-buefy', { css: false, materialDesignIcons: false }],
+  ],
+  /*
+  ** Build configuration
+  */
+  build: {
+    /*
+    ** Run ESLint on save
+    */
+    extend (config, { isDev, isClient }) {
+      if (isDev && isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+      }
+    }
+  }
+}
+
