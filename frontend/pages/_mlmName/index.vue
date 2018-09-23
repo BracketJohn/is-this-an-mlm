edde19fdae6535d1ff3942a58583a807b733a930
nils <nils@fedger.io>
2018-09-23 16:46:49 +0200

    hotfix social rich sharing

--- frontend/pages/_mlmName/index.vue
+++ frontend/pages/_mlmName/index.vue
@@ -93,7 +93,7 @@ import axios from 'axios';
 export default {
   methods: {
     copyUrlToClipboard() {
-      let urlToCopy = `${context.env.baseUrl}/${this.companyName}`;
+      let urlToCopy = `http://www.isthisanmlm.com/${this.companyName}`;
       let self = this;
 
       this.$router.replace(this.companyName);
@@ -114,7 +114,7 @@ export default {
     suggestMlm() {
       let self = this;
       let compName = this.companyName;
-      axios.get(`${context.env.apiUrl}/${compName}`)
+      axios.get(`http://www.isthisanmlm.com:8080/mlms/${compName}`)
         .then(function (response) {
           if (response.data.status == 'success') {
             self.$toast.open({
@@ -142,7 +142,7 @@ export default {
       title: `Is ${this.companyName} an MLM Scheme?`,
       meta: [
         { hid: '0', property: 'og:title', content: `Is ${this.companyName} an MLM Scheme?` },
-        { hid: '1', property: 'og:image', content: `${context.env.baseUrl}/yes.png` },
+        { hid: '1', property: 'og:image', content: `http://www.isthisanmlm.com/yes.png` },
         { hid: '2', property: 'og:description', content: `Is the company ${this.companyName} an MLM scheme? MLM schmes often prey on their members.` }
       ]
     }
